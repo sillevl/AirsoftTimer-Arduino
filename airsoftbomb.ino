3fb18bc927b118c558a8792060e50578bc865afe
Sille Van Landschoot <info@sillevl.be>
2012-11-01 14:47:48 +0100

    Cleanup

--- airsoftbomb.ino
+++ airsoftbomb.ino
@@ -1,24 +1,42 @@
+
 #include <LiquidCrystal.h>
-#include "Beeper.h"
+#include <Wire.h>
+#include <Keypad_I2C.h>
+#include <Keypad.h>
 
+#include "Game.h"
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#define I2CADDR 0x38
+
+LiquidCrystal lcd(13, 12, 11, 9, 10, 8);
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {1, 6, 5, 3}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {2, 0, 4}; //connect to the column pinouts of the keypad
+
+Keypad_I2C kpd( makeKeymap(keys), rowPins, colPins, ROWS, COLS, I2CADDR );
+
+Game* game;
 
 void setup()   
-{                
+{
   Serial.begin(9600);
-  lcd.begin(20, 4);
-  
-  Beeper beeper(6);
+  Serial.print("Hello Serial for PIC");
 
-  lcd.print("hello FreeRTOS!");
-
+  game = new Game(&lcd, &kpd, 6,5);
 }
 
 
 
 void loop()                     
-{
-
+{   
+    game->loop();
 }
 
